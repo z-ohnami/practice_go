ca55267888f6cd7d2c958faabaefce40260a79c8
Makoto Ohnami <z.ohnami@gmail.com>
2017-05-10 22:58:16 +0900

    map 2

--- src/maps/main.go
+++ src/maps/main.go
@@ -17,13 +17,23 @@ func main() {
 
 
 	m = map[string]Vertex{
-		"Bell Labs": Vertex{
-			40.68433, -74.39967,
-		},
-		"Google": Vertex{
-			37.42202, -122.08408,
-		},
+		"Bell Labs": {40.68433, -74.39967, },
+		"Google": {37.42202, -122.08408 },
 	}
 
 	fmt.Println(m)
+
+	q := make(map[string]int)
+
+	q["Answer"] = 42
+	fmt.Println("The value:", q["Answer"])
+
+	q["Answer"] = 48
+	fmt.Println("The value:", q["Answer"])
+
+	delete(q, "Answer")
+	fmt.Println("The value:", q["Answer"])
+
+	v, ok := q["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }

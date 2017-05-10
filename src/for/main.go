6768c07f6a089e25914231625349f0056bd48644
Makoto Ohnami <z.ohnami@gmail.com>
2017-05-10 22:27:36 +0900

    slice and maps

--- src/for/main.go
+++ src/for/main.go
@@ -21,4 +21,17 @@ func main() {
 		sum += sum
 	}
 	fmt.Println(sum)
+
+	pow := []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+	pow = make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
 }
